0cc24658776e9d03c8814961f32eefb2c2a70690
Mladen Nedev <mlados43@gmail.com>
2022-10-04 18:56:03 +0300

    Movie cards styling update

--- RentAMovie/Models/MovieModuls/PopularMovieResultModule.cs
+++ RentAMovie/Models/MovieModuls/PopularMovieResultModule.cs
@@ -7,7 +7,7 @@
         public string Title { get; set; }
 
         [JsonProperty("overview")]
-        public string? Description { get; set; }
+        public string Description { get; set; }
 
         [JsonProperty("release_date")]
         public DateTime ReleaseDate { get; set; }
