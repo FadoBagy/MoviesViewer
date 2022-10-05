4ec50c60384fa55fa7d915e8f452002a8b1a0c2f
Mladen Nedev <mlados43@gmail.com>
2022-10-06 02:34:22 +0300

    Adding carousel slider concept in index view

--- RentAMovie/Controllers/HomeController.cs
+++ RentAMovie/Controllers/HomeController.cs
@@ -1,8 +1,9 @@
 ﻿namespace RentAMovie.Controllers
 {
     using Microsoft.AspNetCore.Mvc;
-    using RentAMovie.Data.Models;
+    using Newtonsoft.Json;
     using RentAMovie.Models;
+    using RentAMovie.Models.MovieModuls;
     using System.Diagnostics;
 
     public class HomeController : Controller
@@ -16,7 +17,31 @@
 
         public IActionResult Index()
         {
-            return View();
+            var apiKey = "api_key=827b5d3636ed4d470d182016543dc5cf";
+            var baseUrl = "https://api.themoviedb.org/3";
+            var mostPopularRequest = baseUrl + "/discover/movie?sort_by=popularity.desc&" + apiKey;
+            var mostPopular21Request = "http://api.themoviedb.org/3/discover/movie?primary_release_date.gte=2021-01-01&primary_release_date.lte=2021-09-31&api_key=827b5d3636ed4d470d182016543dc5cf";
+            var topRatedActionsRequest = "http://api.themoviedb.org/3/discover/movie?with_genres=28&sort_by=vote_average.desc&vote_count.gte=500&api_key=827b5d3636ed4d470d182016543dc5cf";
+
+            var movies = new List<PopularMovieResultModule>();
+            using (var httpClient = new HttpClient())
+            {
+                var endpoint = new Uri(topRatedActionsRequest);
+                var result = httpClient.GetAsync(endpoint).Result;
+                var json = result.Content.ReadAsStringAsync().Result;
+
+                var movieDto = JsonConvert.DeserializeObject<PopularMovieModule>(json);
+                if (movieDto != null && movieDto.Results != null)
+                {
+                    foreach (var movie in movieDto.Results)
+                    {
+                        movies.Add(movie);
+                    }
+                }
+            }
+
+            var moviesTop5 = movies.Take(5);
+            return View(moviesTop5);
         }
 
         public IActionResult Privacy()
