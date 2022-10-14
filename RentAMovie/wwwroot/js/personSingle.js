6b2ddbe35ae8cc841f5cc2e0a601542d8af41272
Mladen Nedev <mlados43@gmail.com>
2022-10-15 02:17:55 +0300

    Styling the page for person's details

---
+++ RentAMovie/wwwroot/js/personSingle.js
@@ -0,0 +1,24 @@
+﻿const biographyPartsEl = document.querySelector('.parts');
+const biographyEl = document.querySelector('.biography');
+
+if (biographyPartsEl.clientHeight > 300) {
+    biographyPartsEl.style.height = '300px';
+
+    let moreBtn = document.createElement('p');
+    moreBtn.textContent = 'Read More';
+    biographyEl.appendChild(moreBtn);
+}
+
+const readMoreEl = document.querySelector('.biography p');
+let isExpanded = false;
+readMoreEl.addEventListener('click', (e) => {
+    if (!isExpanded) {
+        isExpanded = true;
+        biographyPartsEl.style.overflow = 'visible';
+        biographyPartsEl.style.height = 'fit-content';
+    } else {
+        isExpanded = false;
+        biographyPartsEl.style.overflow = 'hidden';
+        biographyPartsEl.style.height = '300px';
+    }
+})
