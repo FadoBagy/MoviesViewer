fd1a7f4c62c942d8573f1339d12a3a898c1993a6
Mladen Nedev <mlados43@gmail.com>
2022-10-19 02:33:19 +0300

    Styling pages and added default poster for movies

--- RentAMovie/Models/MovieModuls/TmdbSingleMovieModel.cs
+++ RentAMovie/Models/MovieModuls/TmdbSingleMovieModel.cs
@@ -8,22 +8,22 @@
         public string Title { get; set; }
 
         [JsonProperty("overview")]
-        public string Description { get; set; }
+        public string? Description { get; set; }
 
         [JsonProperty("release_date")]
-        public DateTime ReleaseDate { get; set; }
+        public DateTime? ReleaseDate { get; set; }
 
         [JsonProperty("poster_path")]
         public string? PosterPath { get; set; }
 
         [JsonProperty("vote_average")]
-        public string Rating { get; set; }
+        public string? Rating { get; set; }
 
         [JsonProperty("id")]
         public int TmdbId { get; set; }
 
         [JsonProperty("vote_count")]
-        public int VoteCount { get; set; }
+        public int? VoteCount { get; set; }
 
         [JsonProperty("backdrop_path")]
         public string? BackdropPath { get; set; }
