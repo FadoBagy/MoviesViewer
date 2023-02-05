44a131ca7d448eb9b2246c11179130b75d29d043
Mladen Nedev <mlados43@gmail.com>
2023-02-05 16:55:24 +0200

    Refactoring

---
+++ RentAMovie/Infrastructure/WebConstants.cs
@@ -0,0 +1,8 @@
+﻿namespace RentAMovie.Infrastructure
+{
+    public class WebConstants
+    {
+        public const string PosterPathCard = "https://image.tmdb.org/t/p/w500";
+        public const string BackdropPath = "https://image.tmdb.org/t/p/w1920_and_h800_multi_faces";
+    }
+}
--- RentAMovie/Infrastructure/WebUtilities.cs
+++ RentAMovie/Infrastructure/WebUtilities.cs
@@ -23,5 +23,44 @@
                 return movieTitle;
             }
         }
+
+        public static string GetRatingColor(string? movieRating)
+        {
+			string rating = "0.0";
+
+			if (movieRating != null)
+			{
+				rating = movieRating;
+			}
+
+			if (float.Parse(rating) >= 8)
+			{
+				return "green";
+			}
+			else if (float.Parse(rating) >= 5)
+			{
+                return "orange";
+			}
+			else if (float.Parse(rating) < 5 && float.Parse(rating) > 0)
+			{
+                return "red";
+			}
+			else
+			{
+                return "black";
+			}
+		}
+
+        public static int? GetReleaseDateYear(DateTime? date)
+        {
+            if (date != null)
+            {
+                return date.Value.Year;
+            }
+            else
+            {
+                return null;
+            }
+        }
     }
 }
